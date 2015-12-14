e090fd319e4942c66e37049ea195a8a23e54e348
jiexishede <chinalilonglong@sina.com>
2015-12-14 23:16:09 +0800

    storyboard practice

---
+++ storyboardParctice/storyboardParctice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  storyboardParctice
+//
+//  Created by 戒惜舍得 on 12/14/15.
+//  Copyright © 2015 戒惜舍得. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ storyboardParctice/storyboardParctice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  storyboardParctice
+//
+//  Created by 戒惜舍得 on 12/14/15.
+//  Copyright © 2015 戒惜舍得. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
